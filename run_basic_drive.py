56c35630fa0ce44391e39687d6060e294eb648db
jerrylu17 <jerry.lu.170@gmail.com>
2020-04-07 15:44:33 -0400

    run_basic_drive and run_to_score initial modifications

---
+++ run_basic_drive.py
@@ -0,0 +1,28 @@
+from run_to_score import run_to_score
+from run_full_drive import run_full_drive
+
+#  states = (A, B, C)
+
+#  Probability breakdown:
+#  P(A_{t+1}, B_{t+1}, C_{t+1} | A_t, B_t, C_t)
+#          = P(A_{t+1} | C_{t+1}, C_t, A_t)
+#              P(B_{t+1} | C_{t+1}, C_t, B_t)
+#              P(C_{t+1} | A_t, B_t, C_t)
+
+# A is YTG, B is Downs, C is LOS- ranges from 0 to 100 (so your own 25 would be 75.  0 would be a touchdown)
+
+# Assume that YG comes from the same simple distribution- this is one of the major changes you would want to make!
+
+
+
+S = [A=10, B=1, C=75] #starting state- better: simulate a kickoff, but this will do for now
+
+
+#now we run our main function.  This function isn't necessarily the one you will use to simulate a game
+#eventually, because it's not necessarily the best "unit" to loop over, but it's something for now!
+#in the very least, you can use it to work out things like punts, and different kinds of scoring opportunities.
+
+run_to_score(S)
+
+#to simulate more, you only need to run the function again.  S does not change, so you don't need to
+#re run the line that defines S.
